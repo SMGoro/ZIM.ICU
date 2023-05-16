44614d0f1f17e448be5fe5310405387dc0fffa7a
SMGoro <72185434+SMGoro@users.noreply.github.com>
2023-05-16 23:26:48 +0800

    Update zh_CN.ebdfefe2.js

--- js/zh_CN.ebdfefe2.js
+++ js/zh_CN.ebdfefe2.js
@@ -1,1 +1,51 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["zh_CN"],{"07b4":function(n,o,t){"use strict";t.r(o);var e=function(){var n=this,o=n.$createElement,t=n._self._c||o;return t("div",{staticClass:"content markdown-body override"},[t("section",{domProps:{innerHTML:n._s(n.content)}})])},c=[],p=t("d0e4"),h=t.n(p),r={data:function(){return{content:h.a}}},l=r,b=(t("4043"),t("2877")),u=Object(b["a"])(l,e,c,!1,null,"15d0b2f8",null);o["default"]=u.exports},"2c53":function(n,o,t){var e=t("a020");e.__esModule&&(e=e.default),"string"===typeof e&&(e=[[n.i,e,""]]),e.locals&&(n.exports=e.locals);var c=t("499e").default;c("0c216c81",e,!0,{sourceMap:!1,shadowMode:!1})},4043:function(n,o,t){"use strict";t("2c53")},a020:function(n,o,t){o=n.exports=t("2350")(!1),o.push([n.i,".content[data-v-15d0b2f8]{position:relative}.content .button-wrapper[data-v-15d0b2f8]{position:absolute;top:30px;right:10px}",""])},d0e4:function(n,o){n.exports='\n'}}]);
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([
+    ["zh_CN"], {
+        "07b4": function (n, o, t) {
+            "use strict";
+            t.r(o);
+            var e = function () {
+                    var n = this,
+                        o = n.$createElement,
+                        t = n._self._c || o;
+                    return t("div", {
+                        staticClass: "content markdown-body override"
+                    }, [t("section", {
+                        domProps: {
+                            innerHTML: n._s(n.content)
+                        }
+                    })])
+                },
+                c = [],
+                p = t("d0e4"),
+                h = t.n(p),
+                r = {
+                    data: function () {
+                        return {
+                            content: h.a
+                        }
+                    }
+                },
+                l = r,
+                b = (t("4043"), t("2877")),
+                u = Object(b["a"])(l, e, c, !1, null, "15d0b2f8", null);
+            o["default"] = u.exports
+        }, "2c53": function (n, o, t) {
+            var e = t("a020");
+            e.__esModule && (e = e.default), "string" === typeof e && (e = [
+                [n.i, e, ""]
+            ]), e.locals && (n.exports = e.locals);
+            var c = t("499e").default;
+            c("0c216c81", e, !0, {
+                sourceMap: !1,
+                shadowMode: !1
+            })
+        }, 4043: function (n, o, t) {
+            "use strict";
+            t("2c53")
+        }, a020: function (n, o, t) {
+            o = n.exports = t("2350")(!1), o.push([n.i, ".content[data-v-15d0b2f8]{position:relative}.content .button-wrapper[data-v-15d0b2f8]{position:absolute;top:30px;right:10px}", ""])
+        }, d0e4: function (n, o) {
+            n.exports = '<h1><a href="http://zim.icu">ZIM.ICU</a></h1>\n<p>“ZIM”工作制，即 <a href="https://github.com/zimzaza4/">ZIMZAZA4</a> 每周六晚放学 6 点到岗，一直工作到周日下午 6 点返校，每周工作 2 天。</p>\n<p>“ZIM”工作制的周工作时间为最低 12x2=24 小时。</p>\n<p><strong>中国大陆工时规管现况（标准工时）：</strong> 一天工作时间为 8 小时，平均每周工时不超过 40 小时；加班上限为一天 3 小时及一个月 36 小时，逾时工作薪金不低于平日工资的 150%。而一周最高工时则为 48 小时。平均每月计薪天数为 21.75 天。</p>\n<p>但是“ZIM”工作制没有工资，甚至还要倒贴XP</p>\n<h2>相关事件报道</h2>\n<ul>\n<li>据本人爆料，ZIM（全名<a href="https://github.com/zimzaza4/">ZIMZAZA4</a> ）作为 <a href="https://imc.re/">IMC.RE</a> Java开发工程师的同时甚至连像样的开发设备都没有，只能拿手机写代码苟且度日，近期陪伴长年的手机甚至用到报废，无奈之下只好更换设备，继续写码。知情人士表示：<strong>五郎，gkd给ZIM买台电脑！</strong></li>\n<li>ZIM 曾高强度参与 <a href="https://github.com/GeyserMC/Geyser">Geyser</a> 相关的开发，率先实现了<a href="https://github.com/zimzaza4/Skript-Floodgate-Api">Skript-Floodgate-Api)</a>  <a href="https://github.com/zimzaza4/GeyserClickableMessageGUI">GeyserClickableMessageGUI</a>   等项目，受到国内外圈内腐竹的瞩目，<del>成为了万众瞩目的秧歌Star</del>，<strong>但是本人并没有从中获取到任何利益</strong>，<del>甚至要打工倒贴给五郎补贴服务器费用（）</del></li>\n<li><a href="https://www.bilibili.com/video/BV1S4411D71b/?spm_id_from=333.337.search-card.all.click&amp;vd_source=6d1e1cc7aaa067da775c4ea996db38f7">震惊！基岩版2b2t服务器华人聚集区被毁，只有一个人的家没被毁！那就是我！<em>哔哩哔哩</em>bilibili</a>：今天刚知道的服务器，然后进来后，一个好心人（zimzaza4）给了我钻石衣服和裤子还有铁剑和铁镐，我玩了一会后下线吃饭，然后刚一上线就被毁了，有个人和那个黑客打，爆了四套钻石套，不过好在现在安定下来了。</li>\n\n</ul>\n<hr />\n<p>什么是 ZIM.ICU？工作狂 ZIM，生病 ICU。</p>\n<p><strong>本网站受到 <a href="https://github.com/996icu/996.ICU">996.ICU</a> 的启发和支持，虽然是整活网站，但是对于 <a href="https://996.icu">996.ICU</a> 请严肃对待。</strong></p>\n<p><strong>Developers&#39; lives matter.</strong></p>\n'
+        }
+    }
+]);
